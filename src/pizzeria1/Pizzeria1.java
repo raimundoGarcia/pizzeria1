05785dcaefad0aa34fdf2ec118935805867e5294
daw <daw@daw-PC08>
2017-03-23 11:01:30 +0100

    blalba

--- src/pizzeria1/Pizzeria1.java
+++ src/pizzeria1/Pizzeria1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pizzeria1;
 
 import javafx.application.Application;
@@ -11,10 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author daw
- */
+
 public class Pizzeria1 extends Application {
     
     @Override
@@ -27,9 +20,7 @@ public class Pizzeria1 extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         launch(args);
     }
